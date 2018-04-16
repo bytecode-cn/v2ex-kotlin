c8060572c09935a65b013585ff03d689fee15800
Echo Wang <dongxu@apache.org>
2018-04-16 21:21:33 +0800

    Add get one topic controller.

--- src/main/kotlin/cn/bytecode/babel/controller/TopicController.kt
+++ src/main/kotlin/cn/bytecode/babel/controller/TopicController.kt
@@ -4,13 +4,20 @@ import cn.bytecode.babel.service.TopicService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class TopicController(val topicService: TopicService){
+class TopicController(val topicService: TopicService) {
 
     @GetMapping("/t")
-    fun topic(map: ModelMap): String {
+    fun topics(map: ModelMap): String {
         map["topics"] = topicService.getTopics()
+        return "topic"
+    }
+
+    @GetMapping("/t/{id}")
+    fun topic(@PathVariable id: String, map: ModelMap): String {
+        map["topic"] = "Hello Topic"
         return "topic"
     }
 
